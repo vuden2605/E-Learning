ade72ad198e26a6800bfd2aeecfc7d11d404520e
vuden2605 <vuden2605@gmail.com>
2025-08-02 17:30:46 +0700

    init project

---
+++ BE/src/test/java/com/example/E_Learning/ELearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.E_Learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
