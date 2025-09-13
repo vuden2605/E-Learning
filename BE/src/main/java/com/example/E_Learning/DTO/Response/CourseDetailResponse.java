8b9edb1b83292eb94395d79f5f4c23a27333cb1c
vuden2605 <vuden2605@gmail.com>
2025-09-13 15:47:30 +0700

    up

--- BE/src/main/java/com/example/E_Learning/DTO/Response/CourseDetailResponse.java
+++ BE/src/main/java/com/example/E_Learning/DTO/Response/CourseDetailResponse.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-//@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CourseDetailResponse {
 	private Long id;
 	private String title;
